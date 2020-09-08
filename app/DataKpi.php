31d56b16d32eb3c44df6fbb6a29970669afbccb2
dennis michael <denniskilawilla@gmail.com>
2020-09-08 11:46:37 +0300

    added kpi model and controller migration missing i skipped a step

---
+++ app/DataKpi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DataKpi extends Model
+{
+    protected $fillable = [
+        // 'topic','description', 'categorie',
+    ];
+}
---
+++ app/Http/Controllers/DataKpiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DataKpi;
+use Illuminate\Http\Request;
+
+class DataKpiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DataKpi  $dataKpi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DataKpi $dataKpi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DataKpi  $dataKpi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DataKpi $dataKpi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DataKpi  $dataKpi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DataKpi $dataKpi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DataKpi  $dataKpi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DataKpi $dataKpi)
+    {
+        //
+    }
+}
