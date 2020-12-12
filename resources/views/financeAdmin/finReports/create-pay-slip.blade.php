d373bc8fcba6a154ba5fc22f9b11b2fa747572a4
dennis michael <denniskilawilla@gmail.com>
2020-12-12 10:25:04 +0300

    add to the financial dashboard

--- resources/views/financeAdmin/finReports/create-pay-slip.blade.php
+++ resources/views/financeAdmin/finReports/create-pay-slip.blade.php
@@ -12,186 +12,198 @@
         <input type="hidden" value="{{ Auth::user()->id }}" name="creator_id">
         <div class="row">
             <div class="form-group col-md-4 input-group-lg">
-                <label for="forKpi_title">Requested by: </label>
-                <input type="text" class="form-control" name="kpi_title"
-                    value="<?= isset($_POST['kpi_title']) ? $_POST['kpi_title'] : ''; ?>" placeholder="" required>
+                <label for="forKpi_title">Payment slip for the month of: <strong> Dec 2020</strong> </label>
+
             </div>
         </div>
         <div class="row">
             <div class="form-group col-md-4 input-group-lg">
-                <label for="forKpi_dc_timing">Staff advance Number: </label>
+                <label for="forKpi_dc_timing">Name: </label>
                 <input type="text" class="form-control" name="dc_timing"
-                    value="<?= isset($_POST['dc_timing']) ? $_POST['dc_timing'] : ''; ?>" placeholder="">
+                    value="<?= isset($_POST['dc_timing']) ? $_POST['dc_timing'] : ''; ?>" placeholder="Bernard">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forKpi_dc_timing">Employee Number: </label>
+                <input type="text" class="form-control" name="dc_timing"
+                    value="<?= isset($_POST['dc_timing']) ? $_POST['dc_timing'] : ''; ?>" placeholder="00345">
             </div>
         </div>
 
 
         <div class="row">
             <div class="form-group col-md-4 input-group-lg">
-                <label for="forunit_of_measure">Project Name: </label>
+                <label for="forunit_of_measure">NSSF number: </label>
                 <input type="text" class="form-control" name="unit_of_measure"
                     value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
             </div>
             <div class="form-group col-md-4 input-group-lg">
                 <label for="forKpi_method_dc">Cost Center: </label>
-                <input type="text" class="form-control" name="method_of_dc"
-                    value="<?= isset($_POST['method_of_dc']) ? $_POST['method_of_dc'] : ''; ?>" placeholder="">
+                <select class="form-control" aria-label="Large" id="selectProject"
+                name="selectProject" aria-describedby="inputGroup-sizing-sm">
+                <option value="select">Currency type</option>
+                <option value="userid1">usd</option>
+                <option value="userid2">tzs </option>
+            </select>
             </div>
         </div>
 
+
         <div class="row">
-            <div class="form-group col-lg-12 input-group-lg">
-                <div class="form-group green-border-focus">
-                    <label for="forPillarBriefDescription">Purpose of Training Summary:
-                    </label>
-                    <textarea class="form-control" name="kpi_desc" rows="10" required></textarea>
-                </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">USD amount: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">current Exchange rate </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">TZs Amount: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">Basic Pay: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">Salary Increment: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">Retention Scheme: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">Gross Salary: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+
+        </div>
+
+         <h2><strong>Less</strong></h2>
+            <br />
+
+        <div class="row">
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">Unpaid Holidays: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">Unpaid Hours: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure"> Net Earnings: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
             </div>
         </div>
 
         <div class="row">
             <div class="form-group col-md-4 input-group-lg">
-                <label for="forKpi_tolerance_band">Departure Date: </label>
-                <input type="date" class="form-control" name="tolerance_band"
-                    value="<?= isset($_POST['tolerance_band']) ? $_POST['tolerance_band'] : ''; ?>" placeholder="">
+                <label for="forunit_of_measure"> Taxable Earnings:  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
             </div>
             <div class="form-group col-md-4 input-group-lg">
-                <label for="forKpi_title">Returning Date: </label>
-                <input type="date" class="form-control" name="last_approved"
-                    value="<?= isset($_POST['last_approved']) ? $_POST['last_approved'] : ''; ?>" placeholder="">
+                <label for="forunit_of_measure"> PAYEE:  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure"> Taxfree allowance - NSSF 10 %:  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
             </div>
         </div>
 
-        <div class="card">
+        <h2><strong>Deductions</strong></h2>
+        <br />
+        <br />
+        <div class="row">
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure"> NSSF:  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure"> Staff Welfare:  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure"> Retention Scheme  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
+        </div>
 
-            <div class="card-content table-responsive">
-                <h4>Add Travel Activities</h4>
-                <br />
-                <div class="row">
-                    <div class="col-lg-12">
-
-                        <div class="form-group col-lg-12 input-group-lg">
-                            <div class="row">
-
-                                <div class="form-group col-lg-12">
-                                    <div class="form-group multiple-form-group" data-max=6>
-                                        <div class="form-group input-group-lg">
-
-                                            <div class="row">
-
-                                                <div class="form-group col-md-4 input-group-lg">
-                                                    <label for="formGroupProductName">Activity</label>
-                                                    <input type="text" class="form-control" id="number_of_breaks"
-                                                        name="number_of_breaks"
-                                                        value="<?= isset($_POST['number_of_breaks']) ? $_POST['number_of_breaks'] : ''; ?>"
-                                                        placeholder="">
-                                                </div>
-
-                                                <div class="form-group col-md-2 input-group-lg">
-                                                    <label for="formGroupProductName">Rate <small> (TZS)</small></label>
-                                                    <input type="text" class="form-control" id="number_of_breaks"
-                                                        name="number_of_breaks"
-                                                        value="<?= isset($_POST['number_of_breaks']) ? $_POST['number_of_breaks'] : ''; ?>"
-                                                        placeholder="">
-                                                </div>
-
-                                                <div class="col-md-1 input-group-lg">
-                                                    <label for="formGroupExampleInput2">Units</label>
-                                                    <input type="text" name="sourceReach[]" class="form-control">
-                                                </div>
-
-
-                                                <div class="col-md-2 input-group-lg">
-                                                    <label for="formGroupExampleInput2">Number</label>
-                                                    <input type="text" name="sourceReach[]" class="form-control">
-
-                                                </div>
-                                                <div class="col-md-2 input-group-lg">
-                                                    <label for="formGroupExampleInput2">Total Cost<small>
-                                                            (TZS)</small></label>
-                                                    <input type="text" name="source[]" class="form-control">
-                                                </div>
-                                                <div class="col-md-1 input-group-lg">
-                                                    <br />
-                                                    <br />
-                                                    <span class="input-group-btn"><button type="button"
-                                                            class="btn btn-outline-primary btn-add">+
-                                                        </button></span>
-                                                </div>
-
-                                            </div>
-                                            <hr style="background-color: aqua;">
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- <label for="exampleFormControlSelect1" class="control-label">select below:</label> -->
-
-                        </div>
-                    </div>
-                    <!-- end of row -->
-                </div>
+        <div class="row">
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure"> Other Deductions  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
             </div>
         </div>
 
 
-        <br />
-        <br />
+        <div class="row">
+            <div class="form-group offset-6 col-md-6 input-group-lg">
+                <h3>Total Deductions: <strong>56,000.00</strong></h3>
+            </div>
+        </div>
         <div class="row">
 
             <div class="form-group offset-6 col-md-6 input-group-lg">
-                <label for="forKpi_title">Total Requested: </label>
-                <input type="text" class="form-control" name="kpi_title"
-                value="<?= isset($_POST['kpi_title']) ? $_POST['kpi_title'] : ''; ?>"
-                placeholder="" required>
+                <h2>Net Pay:<strong>566,000.00</strong></h2>
             </div>
-
+        </div>
+        <div class="row">
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure">Bank:  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
         </div>
 
-
-        <div class="form-group col-md-6 input-group-lg">
-            {{-- <label for="formGroupProductName">to Be reviewed by</label> --}}
-            <select class="form-control" aria-label="Large" id="selectProject"
-                name="selectProject" aria-describedby="inputGroup-sizing-sm">
-                <option value="select">To be reviewed by</option>
-                <option value="userid1">Chacha</option>
-                <option value="userid2">Nguza </option>
-            </select>
-        </div>
-
-
-
-        <div class="form-group col-md-6 input-group-lg">
-            {{-- <label for="formGroupProductName">to Be reviewed by</label> --}}
-            <select class="form-control" aria-label="Large" id="selectProject"
-                name="selectProject" aria-describedby="inputGroup-sizing-sm">
-                <option value="select">To be approved by</option>
-                <option value="userid1">Chacha</option>
-                <option value="userid2">Nguza </option>
-            </select>
+        <div class="row">
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure"> Branch: </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
+            </div>
         </div>
 
 
         <div class="row">
-            <div class="card">
-                <div class="card-content table-responsive">
-                    <div class="form-group col-lg-12 input-group-lg">
-                        <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">I declare to receive funds and agree to retire it within 10 working days after implementation of activities.</label>
-                          </div>
-                    </div>
-                </div>
+            <div class="form-group col-md-4 input-group-lg">
+                <label for="forunit_of_measure"> Account Number:  </label>
+                <input type="text" class="form-control" name="unit_of_measure"
+                    value="<?= isset($_POST['unit_of_measure']) ? $_POST['unit_of_measure'] : ''; ?>" placeholder="">
             </div>
-            </div>
+        </div>
 
-            <br />
-            <br />
 
         <div class="row">
             <div class="form-group offset-md-8 col-md-4 input-group-lg push-right">
-                <button type="submit" class="btn btn-outline-info btn-lg">add new key perfomance indicator</button>
+                <button type="submit" class="btn btn-outline-info btn-lg"> Create Pay Slip</button>
             </div>
             <div class="form-group offset-md-8 col-md-4 input-group-lg pull-right">
                 <button type="button" name="btnCancel" class="btn btn-outline-danger btn-lg">cancel entry</button>
